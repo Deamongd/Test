b27c996a4c84994296500e6bc0cedbff5a61e007
deamon <deamongd@gmail.com>
2019-10-18 16:25:24 +0300

    First array

--- Test.js
+++ Test.js
@@ -1,6 +1,3 @@
-console.log("Hello World");
-var myArray = [1, 2, 3, 4, 6];
-var catNamesArray = ["Jacqueline", "Sophia", "Autumn"];
-//Arrays in JavaScript can hold different types of data, as shown above.
-console.log(myArray[(myArray.length - 1)])
-console.log(myArray.length)
+var fruits = ['Apple', 'Banana'];
+
+console.log(fruits.length);
